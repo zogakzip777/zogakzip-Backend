ba946c279ff3016cc9fc034f2ecffec5aff73319
haeun228 <123haeun@naver.com>
2024-08-12 13:48:24 +0900

    feat: update imageRoutes & imageController

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const imageController = require('../controllers/imageController');
 
-/* multer 설정 */
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/'); // 업로드된 파일이 저장될 디렉토리
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // 파일 이름 설정
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // 이미지 업로드
-router.put('/', imageController.updateComment);
+router.post('/', upload.single('image'), imageController.uploadImage);
 
 module.exports = router;
