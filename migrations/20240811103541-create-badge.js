33b4474500ac76da830570a16b4e2ed09ef0c30a
haeun228 <123haeun@naver.com>
2024-08-14 10:31:33 +0900

    feat: update Badge model

--- migrations/20240811103541-create-badge.js
+++ migrations/20240811103541-create-badge.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable('Badges', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
--- migrations/20240812070356-insert-badge.js
+++ migrations/20240812070356-insert-badge.js
@@ -5,18 +5,23 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Badges', [
       {
+        id: 1,
         name: "7일 연속 추억 등록"
       },
       {
+        id: 2,
         name: "추억 수 20개 이상 등록"
       },
       {
+        id: 3,
         name: "그룹 생성 후 1년 달성"
       },
       {
-        name: "그룹 공간 1만 개 이상 받기"
+        id: 4,
+        name: "그룹 공감 1만 개 이상 받기"
       },
       {
+        id: 5,
         name: "추억 공감 1만 개 이상 받기"
       }
     ]);
