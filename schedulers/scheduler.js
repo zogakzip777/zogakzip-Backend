3392708c75c404b0a63094288d1fbe0275bb0b46
haeun228 <123haeun@naver.com>
2024-08-22 17:36:13 +0900

    refactor: improve badge awarding logic

--- schedulers/scheduler.js
+++ schedulers/scheduler.js
@@ -1,17 +1,13 @@
 const cron = require('node-cron');
-const { checkGroupAge, awardBadge } = require('../services/badgeService.js');
+const { checkGroupAge } = require('../services/badgeService.js');
 const { Group } = require('../models/index.js');
 
 cron.schedule('0 0 * * *', async () => {
   try {
     const groups = await Group.findAll();
-    for (const group of groups) {
-      if (await checkGroupAge(group.id)) {
-        await awardBadge(group.id, 3);
-      }
-    }
+    await Promise.all(groups.map(group => checkGroupAge(group.id)));
   } catch (error) {
-    console.error("배지 부여 실패");
+    console.error('배지 부여 실패: ', error);
   }
 });
 
--- services/badgeService.js
+++ services/badgeService.js
@@ -2,20 +2,18 @@ const db = require('../models/index.js');
 const { Group, GroupBadge, Post } = db;
 const { Op, fn, col, literal } = require('sequelize');
 
-const badges = {
-  1: "7일 연속 추억 등록",
-  2: "추억 수 20개 이상 등록",
-  3: "그룹 생성 후 1년 달성",
-  4: "그룹 공감 1만 개 이상 받기",
-  5: "추억 공감 1만 개 이상 받기"
-};
+// Badge 아이디
+const BADGE_CONSECUTIVE_DAYS_7 = 1; // "7일 연속 추억 등록"
+const BADGE_POST_COUNT_20 = 2; // "추억 수 20개 이상 등록"
+const BADGE_GROUP_AGE_1_YEAR = 3; // "그룹 생성 후 1년 달성"
+const BADGE_GROUP_LIKE_COUNT_10000 = 4; // "그룹 공감 1만 개 이상 받기"
+const BADGE_POST_LIKE_COUNT_10000 = 5; // "추억 공감 1만 개 이상 받기"
 
 const awardBadge = async (groupId, badgeId) => {
   await GroupBadge.findOrCreate({
     where: { groupId, badgeId },
     defaults: { groupId, badgeId }
   });
-  console.log(groupId, badges[badgeId])
 };
 
 const checkConsecutiveDays = async (groupId) => {
@@ -34,14 +32,20 @@ const checkConsecutiveDays = async (groupId) => {
     group: [fn('DATE', col('createdAt'))],
     having: literal('COUNT(DISTINCT DATE(createdAt)) = 7')
   });
-  return posts.length > 0;
+
+  if (posts.length > 0) {
+    await awardBadge(groupId, BADGE_CONSECUTIVE_DAYS_7);
+  }
 };
 
 const checkPostCount = async (groupId) => {
-  const posts = await Post.count({
+  const postCount = await Post.count({
     where: { groupId }
   });
-  return posts.length >= 20;
+
+  if (postCount >= 20) {
+    await awardBadge(groupId, BADGE_POST_COUNT_20);
+  }
 };
 
 const checkGroupAge = async (groupId) => {
@@ -51,15 +55,20 @@ const checkGroupAge = async (groupId) => {
   const currentDate = new Date();
   const createdAt = group.createdAt;
   const oneYearLater = new Date(createdAt);
-  oneYearLater.setFullYear(oneYearLater.getFullYear());
-  return currentDate >= oneYearLater;
+  oneYearLater.setFullYear(oneYearLater.getFullYear()+1);
+  
+  if (currentDate >= oneYearLater) {
+    await awardBadge(groupId, BADGE_GROUP_AGE_1_YEAR);
+  }
 };
 
 const checkGroupLikeCount = async (groupId) => {
   const group = await Group.findByPk(groupId, {
     attributes: ['likeCount']
   });
-  return group.likeCount >= 10000;
+  if (group.likeCount >= 10000) {
+    await awardBadge(groupId, BADGE_GROUP_LIKE_COUNT_10000)
+  }
 };
 
 const checkPostLikeCount = async (groupId) => {
@@ -71,11 +80,13 @@ const checkPostLikeCount = async (groupId) => {
       }
     }
   });
-  return posts.length > 0;
+  
+  if (posts.length > 0) {
+    await awardBadge(groupId, BADGE_POST_LIKE_COUNT_10000);
+  }
 };
 
 module.exports = {
-  awardBadge,
   checkConsecutiveDays,
   checkPostCount,
   checkGroupAge,
